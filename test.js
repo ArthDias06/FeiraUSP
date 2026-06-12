5148b11842b981c91ec5abbb9480e0ae599646b3
Felipe G. Socolowski <felipe.socolowski@gmail.com>
2026-06-12 14:43:15 -0300

    Alterações no CSS para estilização

--- test.js
+++ test.js
@@ -1,27 +1,25 @@
+let page = document.getElementsByTagName("main")[0];
+let footer = document.getElementsByTagName("footer")[0];
+let aside = document.getElementsByTagName("aside")[0];
+let button = document.getElementsByTagName("button")[0];
+let spans = document.getElementsByTagName("span");
+
 function openNav(){
-    let aside = document.getElementsByTagName("aside")[0]
-    let spans = document.getElementsByTagName("span")
-    if(aside.style.width === "10%"){
-        document.getElementsByTagName("aside")[0].style.width = "0px";
-        document.body.style.backgroundColor = "#fff";
-        spans[0].style.backgroundColor = "black";
-        spans[2].style.backgroundColor = "black";
+
+    if (aside.style.width == "15rem") {
+        aside.style.width = "0";
         spans[0].style.transform = "rotate(0deg)";
         spans[2].style.transform = "rotate(0deg)";
         spans[1].style.display = "block";
+    } else {
+        aside.style.width = "15rem";
+        spans[1].style.display = "none";
+        spans[0].style.transform = "translateY(0.425rem) rotate(-45deg)";
+        spans[2].style.transform = "translateY(-0.425rem) rotate(45deg)";
     }
 
-    else{
-        document.getElementsByTagName("aside")[0].style.width = "10%";
-        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-        spans[0].style.backgroundColor = "white";
-        spans[1].style.display = "none";
-        spans[2].style.backgroundColor = "white";
-        spans[0].style.transform = "translateY(4px) rotate(-45deg)";
-        spans[2].style.transform = "translateY(-4px) rotate(45deg)";
-    }
-    
+    page.classList.toggle('darkened');
+    footer.classList.toggle('darkened');
 }
 
-let button = document.getElementsByTagName("button");
-button[0].addEventListener("click", openNav);
+button.addEventListener("click", openNav);
